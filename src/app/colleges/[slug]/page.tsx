71346ff6873ad0c596aec886124581b69b8f645d
08Satyamkumar <satyaaariv@gmail.com>
2026-09-17 11:30:27 +0530

    style: Minimize gap between cards to tight 1-line spacing

--- src/app/colleges/[slug]/page.tsx
+++ src/app/colleges/[slug]/page.tsx
@@ -1057,12 +1057,12 @@ export default function CollegeDetailPage() {
       </div>
 
       {/* 3. CORE TWO-COLUMN CONTENT GRID */}
-      <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="grid grid-cols-1 lg:grid-cols-10 gap-4 lg:gap-5 pt-1">
         {/* LEFT COLUMN: ACTIVE TAB CONTENT (70%) */}
-        <div className="lg:col-span-7 space-y-6">
+        <div className="lg:col-span-7 space-y-2.5">
           {/* TAB 1: COLLEGE INFO */}
           {activeTab === "info" && (
-            <div className="space-y-6">
+            <div className="space-y-2.5">
               {/* 1. TABLE OF CONTENTS (WORLD-CLASS PREMIUM REFERENCE TEMPLATE) */}
               {(() => {
                 const tocList = getCollegeTocList(collegeData);
